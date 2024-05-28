07f3ae0185cf3d3aa9690994b613daaef080457b
Madli <madli.petuhhov89@gmail.com>
2024-05-28 16:34:08 +0300

    database fix

--- database/3_import.sql
+++ database/3_import.sql
@@ -4,16 +4,16 @@ INSERT INTO suvepidu.role (id, name)
 VALUES (default, 'korraldaja');
 
 INSERT INTO suvepidu."user" (id, role_id, username, password, status)
-VALUES (default, 1, 'maarek', '123', 'A');
+VALUES (default, 1, 'kasutaja', '123', 'A');
 INSERT INTO suvepidu."user" (id, role_id, username, password, status)
 VALUES (default, 2, 'korraldaja', '123', 'A');
 INSERT INTO suvepidu."user" (id, role_id, username, password, status)
-VALUES (default, 1, 'rain', '123', 'D');
+VALUES (default, 1, 'mitteaktiivne', '123', 'D');
 
 INSERT INTO suvepidu.business (id, user_id, company_name, phone, email, registry_code, vat_number, status)
-VALUES (default, 2, 'maarek OÜ', '123', 'maarek@maarek.ee', '123', '123', 'A');
+VALUES (default, 2, 'Peod OÜ', '123', 'pidu@mpeod.ee', '123', '123', 'A');
 INSERT INTO suvepidu.business (id, user_id, company_name, phone, email, registry_code, vat_number, status)
-VALUES (default, 2, 'riigo OÜ', '321', 'riigo@riigo.ee', '321', '321', 'A');
+VALUES (default, 2, 'Festivalid OÜ', '321', 'festival@festival.ee', '321', '321', 'A');
 
 INSERT INTO suvepidu.feature (id, name)
 VALUES (default, 'parkimine');
